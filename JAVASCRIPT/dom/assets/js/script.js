225432af0b96ad325d89f66e41bf1ad59692ba1b
Annalisando <anna.csclemos@senacsp.edu.br>
2026-06-27 11:10:22 -0300

    Atualizando

---
+++ JAVASCRIPT/dom/assets/js/script.js
@@ -0,0 +1,10 @@
+// alert('estou aqui');
+
+let titulo = document.getElementById('titulo');
+
+    titulo.innerText = 'teste';
+    titulo.style.color = 'red';
+    titulo.style.fontFamily = "Arial, Helvetica, sans-serif";
+    titulo.style.fontSize = '60';
+
+console.log(titulo);
